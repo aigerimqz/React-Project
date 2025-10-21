4c6cb3ee010e38d01098b4077af43329da6228be
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-21 15:10:07 +0500

    added main navigation structure

--- react-project/src/components/Navigation.jsx
+++ react-project/src/components/Navigation.jsx
@@ -1,5 +1,17 @@
 export default function Navigation(){
     return (
-        <></>
+        <>
+        <div className="nav__bar">
+            <div className="nav__title">
+
+            </div>
+            <div className="nav__links">
+                <a className="nav__item" href="">Home</a>
+                <a className="nav__item" href="">About</a>
+                <a className="nav__item" href="">Tours</a>
+                <a className="nav__item" href="">Contact</a>
+            </div>
+        </div>
+        </>
     )
 }
--- react-project/src/pages/Home.jsx
+++ react-project/src/pages/Home.jsx
@@ -1,10 +1,9 @@
+import Navigation from "../components/Navigation";
+
 export default function Home(){
     return (
         <>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, optio.
-                 Non rerum, consequuntur odio tempore amet harum similique deleniti
-                  ab quo nihil! Quasi, rem debitis. Impedit accusantium expedita quisquam quis.
-            </p>
+            <Navigation></Navigation>
         </>
     )
 }
