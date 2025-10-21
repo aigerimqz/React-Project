9e371e3b2e84e891150c5fc5eb7e830a9f29d2c0
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-21 15:03:16 +0500

    added main components and pages jsx's which i will use in future process(will be updated)

---
+++ react-project/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    return (
+        <>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, optio.
+                 Non rerum, consequuntur odio tempore amet harum similique deleniti
+                  ab quo nihil! Quasi, rem debitis. Impedit accusantium expedita quisquam quis.
+            </p>
+        </>
+    )
+}
