769e3a53e4cc8e5d8b7c91052311d85d5c0e9a40
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-21 15:17:01 +0500

    main structure for home page

--- react-project/src/pages/Home.jsx
+++ react-project/src/pages/Home.jsx
@@ -3,7 +3,21 @@ import Navigation from "../components/Navigation";
 export default function Home(){
     return (
         <>
-            <Navigation></Navigation>
+            <div className="home__block">
+                <Navigation></Navigation>
+                <div className="home__middle">
+                    <div className="home__left">
+                        <h1 className="home__title">Let's Travel</h1>
+                        <p className="home__info">Lorem ipsum dolor sit amet consectetur adipisicing 
+                        elit. Molestiae, magni fugit nihil architecto
+                        maiores mollitia id, cum veritatis quam itaque,
+                        eveniet reprehenderit maxime dolore ex corporis 
+                        soluta suscipit placeat ad.</p>
+                        <button className="home__btn">Explore</button>
+                    </div>
+                    
+                </div>
+            </div>
         </>
     )
 }
