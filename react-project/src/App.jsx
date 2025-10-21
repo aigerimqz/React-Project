af9749b212ef6889a2160547cb95212e9fdff98b
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-21 15:27:20 +0500

    added about page elements

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Home from './pages/Home'; 
+import About from './pages/About';
+import Tours from './pages/Tours';
 function App() {
   
 
   return (
     <>
       <Home></Home>
+      <About></About>
+      <Tours></Tours>
     </>
   )
 }
