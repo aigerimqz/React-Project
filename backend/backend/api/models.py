65d4f7979b800873f2349b3f3e8e0c3f5753be7c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-23 05:49:41 +0500

    backend: added views, serializers and main models for backend, made migrations

--- backend/backend/api/models.py
+++ backend/backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Tour(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.URLField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    duration = models.IntegerField(help_text="Duration of the tour")
+
+    def __str__(self):
+        return f"{self.title} - {self.country}"
--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Tour
+from .serializers import TourSerializer
 
-# Create your views here.
+class TourViewSet(viewsets.ModelViewSet):
+    queryset = Tour.objects.all()
+    serializer_class = TourSerializer
