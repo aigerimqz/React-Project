a3fcb379dff9ca0edcc06de76c84af8c5a4b8c30
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-24 11:58:20 +0500

    styles for TourItem

--- react-project/src/components/TourItem.jsx
+++ react-project/src/components/TourItem.jsx
@@ -1,12 +1,13 @@
+import "../styles/TourItem.css"
 export default function TourItem({ tour }) {
   return (
     <div className="tour__card">
-      <img src={tour.photo} alt={tour.name} className="tour-image" />
-      <div className="tour-content">
-        <h2 className="tour-title">{tour.name}</h2>
-        <p className="tour-country">{tour.country}</p>
-        <p className="tour-description">{tour.description}</p>
-        <p className="tour-price">{tour.price.toLocaleString()} ₸</p>
+      <img src={tour.photo} alt={tour.name} className="tour__image" />
+      <div className="tour__content">
+        <h2 className="tour__title">{tour.name}</h2>
+        <p className="tour__country">{tour.country}</p>
+        <p className="tour__description">{tour.description}</p>
+        <p className="tour__price">{tour.price.toLocaleString()} ₸</p>
       </div>
     </div>
   );
