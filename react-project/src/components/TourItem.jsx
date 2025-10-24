c16c67fc3eb669202fec0b827ccebf54609b6242
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-10-24 11:48:31 +0500

    added strcucture to the touritem

--- react-project/src/components/TourItem.jsx
+++ react-project/src/components/TourItem.jsx
@@ -1,3 +1,13 @@
-export default function TourItem(){
-    
+export default function TourItem({ tour }) {
+  return (
+    <div className="tour__card">
+      <img src={tour.photo} alt={tour.name} className="tour-image" />
+      <div className="tour-content">
+        <h2 className="tour-title">{tour.name}</h2>
+        <p className="tour-country">{tour.country}</p>
+        <p className="tour-description">{tour.description}</p>
+        <p className="tour-price">{tour.price.toLocaleString()} ₸</p>
+      </div>
+    </div>
+  );
 }
--- react-project/src/components/TourList.jsx
+++ react-project/src/components/TourList.jsx
@@ -1,39 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import TourItem from "./TourItem";
 
-export default function TourList(){
+export default function TourList() {
     const [items, setItems] = useState([]);
 
     async function load() {
-        const result = await fetch(
-            "http://127.0.0.1:8000/api/tours/"
-        );
+        const result = await fetch("http://127.0.0.1:8000/api/tours/");
         const data = await result.json();
         console.log(data, "fetched data");
         setItems(data);
-        
     }
 
-    // useEffect( () => {
-    //     load();
-    // }, []);
-    
-    
+    useEffect(() => {
+        load();
+    }, []);
 
     return (
         <div className="tour__list">
             <h2 className="tour__title">Tours</h2>
             <button onClick={load}>Click</button>
-            
-            {items ? 
+            {items.length > 0 ? (
                 <ul className="tour__items">
-                {items.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                    // <p>{item}</p>
-                ))}
-
+                    {items.map((item) => (
+                        <TourItem key={item.id} tour={item} />
+                    ))}
                 </ul>
-            : <p>No data</p>}
-        </div>   
-    )
-
+            ) : (
+                <p>No data</p>
+            )}
+        </div>
+    );
 }
