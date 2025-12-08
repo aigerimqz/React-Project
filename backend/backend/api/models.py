182e82120678f532d162c32b6011bc033553584a
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-12-08 13:48:04 +0500

    back: add new fileds to models

--- backend/backend/api/models.py
+++ backend/backend/api/models.py
@@ -1,16 +1,48 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.text import slugify
+
+class Continent(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
 
 class Tour(models.Model):
     name = models.CharField(max_length=100)
+    continent = models.ForeignKey(Continent, on_delete=models.CASCADE, related_name="tours", null=True, blank=True)
+    categories = models.ManyToManyField(Category, related_name="tours")
+
     country = models.CharField(max_length=100)
     description = models.TextField()
     photo = models.URLField(blank=True, null=True)
+    gallery = models.JSONField(default=list, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     start_date = models.DateField()
     end_date = models.DateField()
     duration = models.IntegerField(help_text="Duration of the tour")
+    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)
 
     def __str__(self):
         return f"{self.name} - {self.country}"
